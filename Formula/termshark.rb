bf8e4b20689b640a445b8a37b847b4e1a81b3c13
Vadim Velicodnii <vadim@velikodniy.name>
2025-12-24 10:32:02 +0000

    Add Linux support to termshark formula

--- Formula/termshark.rb
+++ Formula/termshark.rb
@@ -7,7 +7,6 @@ class Termshark < Formula
   version "2.4.0"
   license "MIT"
 
-  depends_on :macos
   depends_on "wireshark"
 
   on_macos do
@@ -18,6 +17,17 @@ class Termshark < Formula
     on_intel do
       url "https://github.com/gcla/termshark/releases/download/v2.4.0/termshark_2.4.0_macOS_x64.tar.gz"
       sha256 "fbbe6232aeac6aeafbfb439629270437a2dc307f6346bc225c37df7f5645f842"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/gcla/termshark/releases/download/v2.4.0/termshark_2.4.0_linux_arm64.tar.gz"
+      sha256 "6dc3003a48291c463f057d0151727e0d21e295d804f082f3d35d72fd94d0a56e"
+    end
+    on_intel do
+      url "https://github.com/gcla/termshark/releases/download/v2.4.0/termshark_2.4.0_linux_x64.tar.gz"
+      sha256 "a28b2ab534ba8bf2297bbe9846281e62aa0010832b75773369e0d48912acef74"
     end
   end
 

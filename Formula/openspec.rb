8f98ad66cdc7d320f006512c518d2e1634d635f3
velikodniy <1735272+velikodniy@users.noreply.github.com>
2026-01-31 01:03:19 +0000

    Update openspec to 1.1.1

--- Formula/openspec.rb
+++ Formula/openspec.rb
@@ -4,8 +4,8 @@
 class Openspec < Formula
   desc "Spec-driven development for AI coding assistants"
   homepage "https://openspec.dev"
-  url "https://registry.npmjs.org/@fission-ai/openspec/-/openspec-1.0.2.tgz"
-  sha256 "f0e95d723d2120f14bdc1ff8f3a587179a177037de44cdad1bb1793ada6a4579"
+  url "https://registry.npmjs.org/@fission-ai/openspec/-/openspec-1.1.1.tgz"
+  sha256 "e943e2137f86bb6772f1a5ec2fb725fd4302b590de0515a0349364ba419e033c"
   license "MIT"
 
   depends_on "node"

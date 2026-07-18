4eab4080135fc0ecbb44b6b7c50a506f370ac0d9
velikodniy <1735272+velikodniy@users.noreply.github.com>
2026-07-18 01:47:44 +0000

    Update antigravity-cli to 1.1.4

--- Formula/antigravity-cli.rb
+++ Formula/antigravity-cli.rb
@@ -4,28 +4,28 @@
 class AntigravityCli < Formula
   desc "Google Antigravity CLI (agy)"
   homepage "https://antigravity.google/"
-  version "1.1.3"
+  version "1.1.4"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.3-5723946948100096/darwin-arm/cli_mac_arm64.tar.gz"
-      sha256 "51627eb3df9a3a9d878ac4edb87b3f4930002f5d9bba3542d56506b60ef212a4"
+      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.4-6277569641840640/darwin-arm/cli_mac_arm64.tar.gz"
+      sha256 "33ca693ecf8874dbb0b7746f588264e947b919c8f785130e60a7f261190d48dd"
     end
     on_intel do
-      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.3-5723946948100096/darwin-x64/cli_mac_x64.tar.gz"
-      sha256 "c12607de7e333c54799b11ccf01b83701f33464b000799ba81662640304d349a"
+      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.4-6277569641840640/darwin-x64/cli_mac_x64.tar.gz"
+      sha256 "9aa56bfeadc83f724acd9e21e5349a688e029cd14592363425e432c57b81eaed"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.3-5723946948100096/linux-arm/cli_linux_arm64.tar.gz"
-      sha256 "453f9c5530877ab6369e2536e576cfab2bbbcb45923a9bc776678142538e419d"
+      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.4-6277569641840640/linux-arm/cli_linux_arm64.tar.gz"
+      sha256 "333199327cc29e21cb8fcc4e19f7c02c462bf41dd876f1328d64e9922c631700"
     end
     on_intel do
-      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.3-5723946948100096/linux-x64/cli_linux_x64.tar.gz"
-      sha256 "7a7239a69b65d3cf3af7e75f27b2ff4e9cce696a7b9a9e5c37c695f1c74eec34"
+      url "https://storage.googleapis.com/antigravity-public/antigravity-cli/1.1.4-6277569641840640/linux-x64/cli_linux_x64.tar.gz"
+      sha256 "aaab42e395cb4e3bfe5ae88994a340865d949f7a9e7f0604ffa2a3f1e8aadbfa"
     end
   end
 
